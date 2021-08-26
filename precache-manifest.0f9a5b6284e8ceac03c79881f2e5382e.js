4978e9fa09ddf6b547016c422d924f2b7585ade9
boioboi16 <aaryan.praveen@gmail.com>
2021-08-26 22:43:38 +0530

    Updates

---
+++ precache-manifest.0f9a5b6284e8ceac03c79881f2e5382e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f86032f7c71f67b039c8e779e1b6dd6",
+    "url": "/boioboi16/IMDB-Clone/index.html"
+  },
+  {
+    "revision": "abaea1a61fe49188d504",
+    "url": "/boioboi16/IMDB-Clone/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "290aa4f4295c4e36967e",
+    "url": "/boioboi16/IMDB-Clone/static/js/2.33260d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boioboi16/IMDB-Clone/static/js/2.33260d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abaea1a61fe49188d504",
+    "url": "/boioboi16/IMDB-Clone/static/js/main.33eeb9b4.chunk.js"
+  },
+  {
+    "revision": "fece043359258e62f946",
+    "url": "/boioboi16/IMDB-Clone/static/js/runtime-main.8e454b7c.js"
+  }
+]);
